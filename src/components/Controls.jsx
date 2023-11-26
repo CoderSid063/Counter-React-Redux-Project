0ed4031d102572cdac6a1c7b5fd471f34c4281ff
CoderSid063 <sudhansupanda063@gmail.in>
2023-11-26 15:15:56 +0530

    add new button

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -1,20 +1,69 @@
+import { useRef } from "react";
 import { useDispatch } from "react-redux";
 
 const Controls = () => {
-const dispatch = useDispatch()
+  const dispatch = useDispatch();
+  const inputElement = useRef();
 
-const handleIncrement= ()=> {
-  dispatch({type : "INCREMENT"})
-}
+  const handleIncrement = () => {
+    dispatch({ type: "INCREMENT" });
+  };
 
-const handleDecremet = ()=> {
-  dispatch({type : "DECREMENT"})
-}
+  const handleDecremet = () => {
+    dispatch({ type: "DECREMENT" });
+  };
+
+  const handleAdd = () => {
+    dispatch({ 
+      type: "ADD", 
+      payload: { num: inputElement.current.value } });
+      inputElement.current.value = "";
+  };
+
+  const handleSubtract = () => {
+    dispatch({
+      type: "SUBTRACT",
+      payload: { num: inputElement.current.value } });
+      inputElement.current.value = "";
+  };
   return (
-    <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
-      <button type="button" className="btn btn-primary" onClick={handleIncrement}>+1</button>
-      <button type="button" className="btn btn-success" onClick={handleDecremet}>-1</button>
-    </div>
+    <>
+      <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
+        <button
+          type="button"
+          className="btn btn-primary"
+          onClick={handleIncrement}
+        >
+          +1
+        </button>
+        <button
+          type="button"
+          className="btn btn-success"
+          onClick={handleDecremet}
+        >
+          -1
+        </button>
+      </div>
+
+      <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
+        <input
+          type="text"
+          className="number-input"
+          placeholder="Enter number"
+          ref={inputElement}
+        />
+        <button type="button" className="btn btn-info" onClick={handleAdd}>
+          ADD
+        </button>
+        <button
+          type="button"
+          className="btn btn-danger"
+          onClick={handleSubtract}
+        >
+          Subtract
+        </button>
+      </div>
+    </>
   );
 };
 export default Controls;

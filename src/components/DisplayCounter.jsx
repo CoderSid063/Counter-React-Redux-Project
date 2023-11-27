e08efe225968c92f585b6af293bd9710cd330cf1
CoderSid063 <sudhansupanda063@gmail.in>
2023-11-27 11:21:33 +0530

    create ,provide ,use of store data using redux-toolkit

--- src/components/DisplayCounter.jsx
+++ src/components/DisplayCounter.jsx
@@ -1,7 +1,13 @@
 import { useSelector } from "react-redux";
 
 const DisplayCounter = () => {
-  const counter = useSelector((store) =>  store.counter);
-  return <p className="lead mb-4">counter Value : {counter}</p>;
+  // const counterObj = useSelector((store) =>  store.counter);
+  // const counter = counterObj.counterVal ;
+
+  // destructering & store."slice name" 
+  const {counterVal} = useSelector((store) => store.counter);
+
+
+  return <p className="lead mb-4">counter Value : {counterVal}</p>;
 };
 export default DisplayCounter;
--- src/store/index.js
+++ src/store/index.js
@@ -1,20 +1,36 @@
-import { createStore } from "redux";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 
-const INITIAL_VALUE = { counter: 0 };
+const counterSlice = createSlice({
+  name: "counter",
+  initialState: { counterVal: 0 },
+  reducer: {
+    increment: (state, action) => {},
+    decrement: (state, action) => {},
+    add: (state, action) => {},
+    subtract: (state, action) => {},
+  },
+});
 
-const counterReducer = (store = INITIAL_VALUE, action) => {
-  if (action.type === "INCREMENT") {
-    return { counter: store.counter + 1 };
-  } else if (action.type === "DECREMENT") {
-    return { counter: store.counter - 1 };
-  } else if (action.type === "ADD"){
-    return {counter : store.counter + Number(action.payload.num)}
-  } else if (action.type === 'SUBTRACT'){
-    return {counter : store.counter - Number(action.payload.num)}
-  }
-  return store;
-};
+const counterStore = configureStore({
+  reducer: {
+    counter: counterSlice.reducer,
+  },
+});
 
-const counterStore = createStore(counterReducer);
-
+export const counterAction = counterSlice.actions;
 export default counterStore;
+
+// const INITIAL_VALUE = { counter: 0 };
+
+// const counterReducer = (store = INITIAL_VALUE, action) => {
+//   if (action.type === "INCREMENT") {
+//     return { counter: store.counter + 1 };
+//   } else if (action.type === "DECREMENT") {
+//     return { counter: store.counter - 1 };
+//   } else if (action.type === "ADD") {
+//     return { counter: store.counter + Number(action.payload.num) };
+//   } else if (action.type === "SUBTRACT") {
+//     return { counter: store.counter - Number(action.payload.num) };
+//   }
+//   return store;
+// };

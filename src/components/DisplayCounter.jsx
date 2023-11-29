f17945cd7983d0694753eb63981d3388148d6e00
CoderSid063 <sudhansupanda063@gmail.in>
2023-11-29 17:39:01 +0530

    normal commit

--- src/components/DisplayCounter.jsx
+++ src/components/DisplayCounter.jsx
@@ -4,7 +4,7 @@ const DisplayCounter = () => {
   // const counterObj = useSelector((store) =>  store.counter);
   // const counter = counterObj.counterVal ;
 
-  // destructering & store."slice name"
+  // destructering & store."slice.name"
   const { counterVal } = useSelector((store) => store.counter);
 
   return <p className="lead mb-4">counter Value : {counterVal}</p>;
